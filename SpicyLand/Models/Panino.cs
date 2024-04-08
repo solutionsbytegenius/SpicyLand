c36acad257545dae0f2a228b7c0f8552167c1620
Nicolò Di Marco <nikox95ct@outlook.it>
2024-04-08 23:31:17 +0200

    fix Problem cast double to decimal

--- SpicyLand/Models/Panino.cs
+++ SpicyLand/Models/Panino.cs
@@ -7,7 +7,7 @@ namespace SpicyLand.Models
         public Guid PaninoID { get; set; }
         public string Nome { get; set; } = "";
         public string Descrizione { get; set; } = "";
-        public double Prezzo { get; set; }
+        public decimal Prezzo { get; set; }
         public DateTime DataCreazione { get; set; }
         public IFormFile Immagine { get; set; }
         public string PathImage { get; set; } = "";
